0e0793b3b8deaf95d21bfba3fc4d96794475f326
Kyran Rana <kyranrana0@gmail.com>
2024-03-29 16:30:03 +0000

    formatting fixes

--- src/main/java/com/thevirtualforge/musicalog/service/AlbumService.java
+++ src/main/java/com/thevirtualforge/musicalog/service/AlbumService.java
@@ -8,9 +8,14 @@ import java.util.List;
 
 public interface AlbumService {
     List<AlbumDTO> getAllAlbums();
+
     AlbumDTO getAlbum(final String key);
+
     List<AlbumDTO> getMatchingAlbums(final AlbumFilterDTO filter);
+
     AlbumDTO createAlbum(final AlbumPayloadDTO payload);
+
     void updateAlbum(final String id, final AlbumPayloadDTO payload);
+
     void deleteAlbum(final String id);
 }
--- src/main/java/com/thevirtualforge/musicalog/service/ImageStoreService.java
+++ src/main/java/com/thevirtualforge/musicalog/service/ImageStoreService.java
@@ -3,6 +3,8 @@ package com.thevirtualforge.musicalog.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageStoreService {
+
     void storeImage(final String key, final MultipartFile file);
+
     void deleteImage(final String key);
 }
